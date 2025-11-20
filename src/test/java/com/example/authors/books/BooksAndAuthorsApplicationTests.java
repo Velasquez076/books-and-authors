ab0c4c7b86d59084e2346c423492fba79066713a
Julián Velásquez <velasquezjulian914@gmail.com>
2025-11-20 12:01:23 -0500

    First commit

---
+++ src/test/java/com/example/authors/books/BooksAndAuthorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authors.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAndAuthorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
